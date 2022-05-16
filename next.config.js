4251a82a3bf5ec4d9b926d425b5bca244a897148
Leandrosales993 <leandrosales993@gmail.com>
2022-05-16 16:46:39 -0300

    Enviando projeto

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+}
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.traction.one'],
+  },
+}
